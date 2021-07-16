8a40e6129eb46cc4c50e3d5f18641659f7bb5950
Anna-Fridman <annafridman81@gmail.com>
2021-07-16 17:19:55 +0300

    fixed everrything. works with useContext and proxy server

--- react-app/src/components/Categories.js
+++ react-app/src/components/Categories.js
@@ -1,3 +1,5 @@
+import './Categories.css';
+import Category from './Category';
 import { useState, useEffect } from "react";
 
 
@@ -7,16 +9,23 @@ export default function Categories(){
                                                    //After fetching, the product object are going to come with a 'category' key.
 
     useEffect(()=> {//Now we are fetching the categories
-        fetch('/categories')//"proxy": "http://localhost:8080" added in package.json file
+        fetch('/categories')
           .then(response => response.json())//Here we got the categories array 
           .then(categoriesArr => setCategories(categoriesArr));//Takes the categories array that we got above and replaces the empty 'categories' array with the one we got from fetch()
         }, []);
     
     return (
-
-        <div>
-            {categories.map(category => <span> <a href="#">{category}</a> </span>)} {/*After fetching the 'categories' array we map it and put it in the DOM*/}
-        </div> 
+            <div className="categories">
+                <h3>Categories</h3>
+                <ul className="categories-list">
+                    {categories.map(category => 
+                    <Category>
+                       <span className="custon-category"> {category}</span> {/*After fetching the 'categories' array we map it and put it in the DOM, category is the child of Category */}
+                    </Category>                                             //So if i want to style it i can wrap it with span and style ir in css
+                                                                             //And then we can add diffrent content to the Category square, the square will look the same but the content will change
+                    )} 
+                </ul>                                                                                                             
+            </div> 
 
     );
 }
