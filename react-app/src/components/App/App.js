349caf073150ea801e970e5b0db8bea2fff4306d
Anna-Fridman <annafridman81@gmail.com>
2021-08-03 11:06:05 +0300

    react Router added, Header component added

--- react-app/src/components/App/App.js
+++ react-app/src/components/App/App.js
@@ -1,6 +1,11 @@
 
   
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 import React from 'react';
 import Cart from './../Cart';
@@ -8,25 +13,30 @@ import Catalog from '../Catalog';
 import Categories from '../Categories';
 import CatalogProvider from '../CatalogContext';
 import CartProvider from '../CartContext';
-
+import Header from "../Header";
 
 function App() {
-
-  
-    return(
-
-    <CatalogProvider>
-    <CartProvider>
-     
+  return(
     <div className="App"> 
-      <Cart /> {/*Using "products" (all the products) array as a prop so we can access it in Cart component */}
-                                                     {/*Using "items" (only cart products) as a prop so we can access it in Cart component  */}
-      <Categories />
-      <Catalog  /> {/*Using "products" array as a prop so we can access it in Catalog component */}
+    <div className="Header"> 
+    <Router>
+      <CatalogProvider>
+      <CartProvider>
+        <Header/>
+        <Switch>
+          <Route path="/products" exact>
+            <Categories />
+            <Catalog  /> {/*Using "products" array as a prop so we can access it in Catalog component */}
                                                       {/*"onAddToCart" is a prop that stores "addToCart" function so we can use it in catalog*/}
+          </Route>
+            <Cart /> {/*Using "products" (all the products) array as a prop so we can access it in Cart component */}
+                                                     {/*Using "items" (only cart products) as a prop so we can access it in Cart component  */}
+        </Switch>
+      </CartProvider>  
+      </CatalogProvider> 
+    </Router>
     </div>
-    </CartProvider>  
-    </CatalogProvider> 
+    </div>
 
     );
 }
--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -1,6 +1,7 @@
 import {
     Link,
   } from "react-router-dom";
+  import './Header.css';
 
 export default function Header(){
 
