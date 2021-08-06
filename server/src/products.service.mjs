a129127a71b72a76b0daee50dc21d88729c9c58c
Anna-Fridman <annafridman81@gmail.com>
2021-08-06 09:25:17 +0300

    filter products by title, maxPrice and by minPrice - added to products.servise.mjs

--- server/src/products.service.mjs
+++ server/src/products.service.mjs
@@ -5,9 +5,31 @@ const {ObjectId}= Mongo;//extracting objectId function from momgodb, the functio
 
 export let products = loadJson.sync('./data/products.json'); //Saving the data  from the wanted file to a variable 
 
-export  function getProducts() {
-    return getProductsCollection().find({}).toArray();
+export function getProducts(filter = {}) {//As default we send an empty object to data base to filter by (so it will get all products)
+     
+    const query = {};//At first query is an empty object, so we get all products. But if the filter i got has a "title" or "maxPrice" or "minPrice" - we filter by them
+
+    if(filter.title){//If the filter is "title" ("http://localhost:8080/products/?title=SOMETHING" )         
+        query.title = new RegExp(filter.title, "i");//The RegExp object is used for matching text with a pattern  (the pattern is"SOMETHING" in this example)
+                                                   //RegExp will match all titles that have "SOMETHING" in their values
+    }                                              //The "i" is RegExps "ignore case", it means it doesn't matter if the expression we filter by is uppercase or not
+
+    if(filter.maxPrice || filter.minPrice){//Return all products that teir price is lower than "SOMENUMBERS" OR bigger than SOMENUMBERS"
+        query.price = {};
+        if(filter.maxPrice) // if the filter is "maxPrice" http://localhost:8080/products/?maxPrice=SOMENUMBERS
+        query.price.$lt = parseInt(filter.maxPrice)//$lt-  is mongoDB method, means "lower than"
+        if(filter.minPrice) //if the filter is "minPrice"  http://localhost:8080/products/?minPrice=SOMENUMBERS
+        query.price.$gt = parseInt(filter.minPrice)//$gt - is mongoDB method, means "geater than"                                          
+                                                     //parseInt - because the "price" value is a string and we want a number
+    }
+
+
+
+    return getProductsCollection()
+    .find(query)//return all querry, but if we used (?title=SOMETHING) returns only products that contain "SOMETHING" in their tilte
+    .toArray();
 }
+
 export  function getProduct(id){
   return  getProductsCollection()
     .findOne({_id: ObjectId(id)});//findOne- is a mongoDB method. finds the product that its _id  is the same as the id we pased in the function
